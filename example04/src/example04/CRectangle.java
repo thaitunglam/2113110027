68cd74af73ea695557ad428f3ca31af49028a940
Admin <Admin@D20908>
2015-05-23 14:35:54 +0700

    bai4

---
+++ example04/src/example04/CRectangle.java
@@ -0,0 +1,16 @@
+package example04;
+
+class CRectangle 
+{
+	int width;
+	int height;
+	public CRectangle (int a,int b)
+	{
+		width = a;
+		height = b;
+	}
+	public int area()
+	{
+		return (width*height);
+	}
+}
